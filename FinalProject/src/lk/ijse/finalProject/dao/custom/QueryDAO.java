e64fc9eac7bfbb15a5295df7a271c9e0775cb160
ravinduKaushaly <ravinduk56@gmail.com>
2023-02-10 12:02:15 +0530

    added query to QueryDAO

--- FinalProject/src/lk/ijse/finalProject/dao/custom/QueryDAO.java
+++ FinalProject/src/lk/ijse/finalProject/dao/custom/QueryDAO.java
@@ -1,6 +1,11 @@
 package lk.ijse.finalProject.dao.custom;
 
 import lk.ijse.finalProject.dao.SuperDAO;
+import lk.ijse.finalProject.dto.OrderDetailDTO;
+
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
 
 public interface QueryDAO extends SuperDAO {
+    OrderDetailDTO getOrderDetail() throws SQLException, ClassNotFoundException;
 }
--- FinalProject/src/lk/ijse/finalProject/dao/custom/impl/QueryDAOImpl.java
+++ FinalProject/src/lk/ijse/finalProject/dao/custom/impl/QueryDAOImpl.java
@@ -1,6 +1,15 @@
 package lk.ijse.finalProject.dao.custom.impl;
 
 import lk.ijse.finalProject.dao.custom.QueryDAO;
+import lk.ijse.finalProject.dto.OrderDetailDTO;
+import lk.ijse.finalProject.utill.CrudUtil;
+
+import java.sql.SQLException;
 
 public class QueryDAOImpl implements QueryDAO {
+    @Override
+    public OrderDetailDTO getOrderDetail() throws SQLException, ClassNotFoundException {
+        return CrudUtil.execute("SELECT orderdetail.cId,orderdetail.orderId,orders.date,orders.stayDayCount,orders.rId,orders.pkgId,orders.regNo FROM orders INNER JOIN\n" +
+                "orderdetail ON orders.orderId = orderdetail.orderId;");
+    }
 }
